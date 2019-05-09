68f4c078ad9234180b4f6e0499ac03874ea553ad
vcaldo <>
2019-05-09 20:45:16 -0300

    fix send msg bug

--- bkpy.py
+++ bkpy.py
@@ -4,6 +4,7 @@ import logging
 import os
 import json
 import time
+from telegram.ext import Updater, MessageHandler
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(name)s [%(levelname)s]: %(message)s")
@@ -11,11 +12,17 @@ cfg = open(os.environ["HOME"] + "/.config/rclone/rclone.conf")
 cfg = cfg.read()
 if not os.path.exists("state/"):
     os.mkdir("state/")
+#src_file = os.environ["SRC_FILE"]
+#tg_token = os.environ["TG_TOKEN"]
+#tg_channel = os.environ["TG_CHANNEL"]
 
+tg_token = "875674671:AAFCbNNPVVJNfDbmVxbVW0OIRLSfpJxvqhA"
+#tg_channel = 1030377622
+tg_channel = "@statuslogs"
 
 def do_bkp(name, params):
     result = rclone.with_config(cfg).sync(
-        params["source"], params["provider"] + ":" + params["dest"])
+        params["source"], params["provider"] + ":" + params["dest"], flags=["--skip-links"])
     if result["code"] == 0:
         if not os.path.exists("state/" + name):
             os.mkdir("state/" + name)
@@ -24,10 +31,8 @@ def do_bkp(name, params):
         lastrun.close()
         copylastrun = rclone.with_config(cfg).copy(
             lastrun.name, params["provider"] + ":" + params["dest"])
-        if copylastrun == 0:
-            print("backup ok, lastrun ok")
-        else:
-            print(copylastrun["error"])
+        if not copylastrun == 0:
+            result = copylastrun
     else:
         print(result["error"])
     return result
@@ -45,15 +50,26 @@ def check_age(name, params):
         return True
 
 
-with open("sourcelist.json") as srcpath:
+def send_msg(name, params, bkp_time):
+    msg = "Backup {} completed in {} minutes.\nNext backup in {} days.".format(
+        name, int(bkp_time/60), params["age"])
+    updater = Updater(token=tg_token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.bot.send_message(chat_id=tg_channel, text=msg)
+
+
+with open("/home/vini/lab/bkpy/sourcelist.json") as srcpath:
     source = json.loads(srcpath.read())
 
 for k, v in source.items():
+    start_time = time.time()
     msg = "Checking backup age for {}.".format(k)
     print(msg)
     check = check_age(k, v)
     if check:
         print("Starting backup for " + k)
         bkp = do_bkp(k, v)
+        total_time = time.time() - start_time
+        if bkp == 0: send_msg(k, v, total_time)
     else:
         print("Backup is up to date")
